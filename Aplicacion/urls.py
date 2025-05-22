e9844f4b4e53109a2780678f981436a4e1b11753
Jonathan Yanez <jon.yanezy@gmail.com>
2025-05-22 00:00:38 -0400

    se agregan metodos para eliminar Fabricantes y Tipo de Vehiculos

--- Aplicacion/urls.py
+++ Aplicacion/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('post_buscar', views.post_buscar, name='post_buscar'),
     path('post_agregar_fabricante', views.post_agregar_fabricante, name='post_agregar_fabricante'),
     path('post_agregar_tipo', views.post_agregar_tipo, name='post_agregar_tipo'),
+    path('post_eliminar_fabricante/<int:fabricante_id>/', views.post_eliminar_fabricante, name='post_eliminar_fabricante'),
+    path('post_eliminar_tipo/<int:tipo_id>/', views.post_eliminar_tipo, name='post_eliminar_tipo')
 ]
--- Aplicacion/views.py
+++ Aplicacion/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post, Fabricante, TipoVehiculo
 from .forms import PostForm, FabricanteForm, TipoVehiculoForm
+from django.shortcuts import redirect, get_object_or_404
 
 
 def post_listar(request):
@@ -39,17 +40,6 @@ def post_buscar(request):
 
 
 
-# def post_agregar_fabricante(request):
-#     if request.method == 'POST':
-#         form = FabricanteForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('Aplicacion:post_listar')
-#     else:
-#         form = FabricanteForm()
-#     return render(request, 'Aplicacion/agregar_fabricante.html', {'form': form})
-
-
 def post_agregar_fabricante(request):
     form = FabricanteForm()
 
@@ -64,21 +54,21 @@ def post_agregar_fabricante(request):
         'form': form,
         'fabricantes': fabricantes
     })
-
-
-
-
-
-
-# def post_agregar_tipo(request):
-#     if request.method == 'POST':
-#         form = TipoVehiculoForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('Aplicacion:post_listar')
-#     else:
-#         form = TipoVehiculoForm()
-#     return render(request, 'Aplicacion/agregar_tipo.html', {'form': form})
+    
+    
+    
+def post_eliminar_fabricante(request, fabricante_id):
+    if request.method == 'POST':
+        fabricante = get_object_or_404(Fabricante, id=fabricante_id)
+        fabricante.delete()
+    return redirect('Aplicacion:post_agregar_fabricante')  
+    
+    
+# def post_eliminar_fabricante(request, fabricante_id):
+#     fabricante = Fabricante.objects.get(id=fabricante_id)
+#     fabricante.delete()
+#     fabricante = Fabricante.objects.all()
+#     return redirect('Aplicacion:post_agregar_fabricante')    
 
 
 
@@ -97,3 +87,12 @@ def post_agregar_tipo(request):
         'form': form,
         'tipos': tipos,
     })
+    
+    
+def post_eliminar_tipo(request, tipo_id):
+    if request.method == 'POST':
+        tipo = get_object_or_404(TipoVehiculo, id=tipo_id)
+        tipo.delete()
+    return redirect('Aplicacion:post_agregar_tipo')      
+    
+    
