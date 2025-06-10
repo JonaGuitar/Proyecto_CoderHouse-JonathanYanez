956ce2ba2ef2b43e56238d7d9d6cff7fe10ac2a1
Jonathan Yanez <jon.yanezy@gmail.com>
2025-06-10 17:15:33 -0400

    se agrega vista detalle_vehiculo a ADMIN

--- Aplicacion/admin.py
+++ Aplicacion/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, TipoVehiculo, Fabricante
+from .models import Post, TipoVehiculo, Fabricante, DetalleVehiculo
 
 admin.site.register(Post)
 admin.site.register(TipoVehiculo)
 admin.site.register(Fabricante)
+admin.site.register(DetalleVehiculo)
